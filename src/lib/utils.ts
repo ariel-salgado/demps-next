0482df55be59c9f5831f7422274c321bf9c1cb72
Ariel Salgado <ariel.salgado.acevedo@gmail.com>
2024-05-14 06:09:51 -0400

    Added cn utility function

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,8 +1,15 @@
 import type { G } from '$lib/types';
+import type { ClassValue } from 'clsx';
 import type { Feature, FeatureCollection } from 'geojson';
 
+import { clsx } from 'clsx';
 import { rewind } from '@turf/rewind';
+import { twMerge } from 'tailwind-merge';
 import { randomPolygon } from '@turf/random';
+
+export function cn(...inputs: ClassValue[]) {
+	return twMerge(clsx(inputs));
+}
 
 export function generatePolygon(): Feature<G> {
 	const id = crypto.randomUUID().split('-').at(-1) as string;
