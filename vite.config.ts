b609b69b399d004778c10df1c989f7a206283fbf
Ariel Salgado <ariel.salgado.acevedo@gmail.com>
2024-05-13 21:40:21 -0400

    Added tailwindcss

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,14 @@
 import { defineConfig } from 'vitest/config';
 import { sveltekit } from '@sveltejs/kit/vite';
 
+import tailwindcss from '@tailwindcss/vite';
+
 export default defineConfig({
-	plugins: [sveltekit()],
+	plugins: [sveltekit(), tailwindcss()],
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
+	},
+	css: {
+		transformer: 'lightningcss'
 	}
 });
