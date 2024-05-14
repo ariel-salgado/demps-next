a107705e2fe339323ededea6c38a0edf44692f2a
Ariel Salgado <ariel.salgado.acevedo@gmail.com>
2024-05-13 21:58:36 -0400

    Added view transition api

--- src/app.d.ts
+++ src/app.d.ts
@@ -8,6 +8,21 @@ declare global {
 		// interface PageState {}
 		// interface Platform {}
 	}
+
+	interface Document {
+		startViewTransition(updateCallback: () => Promise<void> | void): ViewTransition;
+	}
+
+	interface ViewTransition {
+		finished: Promise<void>;
+		ready: Promise<void>;
+		updateCallbackDone: Promise<void>;
+		skipTransition(): void;
+	}
+
+	interface CSSStyleDeclaration {
+		viewTransitionName: string;
+	}
 }
 
 export {};
