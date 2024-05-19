26b3693c08c31cc5913aa92ebbd0fe085b011aa5
Ariel Salgado <ariel.salgado.acevedo@gmail.com>
2024-05-19 19:35:41 -0400

    Added main layout

---
+++ src/routes/(app)/+layout.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	let { children } = $props();
+</script>
+
+<div class="grid min-h-screen grid-rows-[auto_1fr_auto]">
+	<header></header>
+
+	<main>
+		{@render children()}
+	</main>
+
+	<footer></footer>
+</div>
