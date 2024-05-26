989be340f2a8f27449db784a93088085d7686a8c
Ariel Salgado <ariel.salgado.acevedo@gmail.com>
2024-05-26 07:09:10 -0400

    Fixed overlayLayer type

--- src/lib/components/leaflet/map/index.ts
+++ src/lib/components/leaflet/map/index.ts
@@ -1,10 +1,12 @@
-import type { Map, FeatureGroup, LayerGroup } from 'leaflet';
+import type { Environment } from '$lib/states/env.svelte';
+import type { Map, FeatureGroup, Control } from 'leaflet';
 
 export { default as Map } from './map.svelte';
 export { contextKey } from './map.svelte';
 
 export type MapContext = {
 	map: Map;
+	environment: Environment;
 	featureGroup: FeatureGroup;
-	overlayLayer: LayerGroup;
+	overlayLayer: Control.Layers;
 };
