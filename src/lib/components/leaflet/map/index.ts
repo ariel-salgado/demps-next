cddec2400faf6663c5b5f80e0ebff99fab830f66
Ariel Salgado <ariel.salgado.acevedo@gmail.com>
2024-05-26 03:11:50 -0400

    Added context

--- src/lib/components/leaflet/map/index.ts
+++ src/lib/components/leaflet/map/index.ts
@@ -1,2 +1,10 @@
+import type { Map, FeatureGroup, LayerGroup } from 'leaflet';
+
 export { default as Map } from './map.svelte';
 export { contextKey } from './map.svelte';
+
+export type MapContext = {
+	map: Map;
+	featureGroup: FeatureGroup;
+	overlayLayer: LayerGroup;
+};
