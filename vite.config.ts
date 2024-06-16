dc57184423d7331a00e0f8c646746a7145590322
Ariel Salgado <ariel.salgado.acevedo@gmail.com>
2024-06-16 05:58:44 -0400

    Minify css build with lightingcss

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,9 @@ export default defineConfig({
 	server: {
 		host: '0.0.0.0'
 	},
+	build: {
+		cssMinify: 'lightningcss'
+	},
 	test: {
 		globals: true,
 		include: ['src/**/*.{test,spec}.{js,ts}']
