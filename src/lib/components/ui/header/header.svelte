ed571577dbbd0cb9b8bd3751eee21ace5e0dda77
Ariel Salgado <ariel.salgado.acevedo@gmail.com>
2024-06-17 01:40:13 -0400

    Added header

---
+++ src/lib/components/ui/header/header.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+	import { Button } from '$lib/components/ui';
+
+	interface NavLinks {
+		label: string;
+		href: string;
+	}
+
+	const navLinks: NavLinks[] = [{ label: 'inicio', href: '/' }];
+</script>
+
+<header class="z-[999] flex h-16 items-center justify-around shadow">
+	<a class="text-3xl font-bold" href="/">DEMPS</a>
+
+	<nav>
+		<ul class="flex items-center gap-x-4">
+			{#each navLinks as { label, href }}
+				<li>
+					<a {href}>{label}</a>
+				</li>
+			{/each}
+		</ul>
+	</nav>
+
+	<div class="flex">
+		<Button>Simular</Button>
+	</div>
+</header>
--- src/routes/(app)/+layout.svelte
+++ src/routes/(app)/+layout.svelte
@@ -1,11 +1,12 @@
 <script lang="ts">
 	import { Toaster } from 'svelte-sonner';
+	import { Header } from '$lib/components/ui';
 
 	const { children } = $props();
 </script>
 
 <div class="grid min-h-screen grid-rows-[auto_1fr_auto]">
-	<header></header>
+	<Header />
 
 	<main>
 		{@render children()}
