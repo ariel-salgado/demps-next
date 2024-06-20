ef7c13e77e6d09b77c6359c6057ac1fb38fe233f
Ariel Salgado <ariel.salgado.acevedo@gmail.com>
2024-06-20 07:51:13 -0400

    Recieving data from chokidar

--- src/routes/(app)/simulacion/+page.svelte
+++ src/routes/(app)/simulacion/+page.svelte
@@ -1,21 +1,63 @@
 <script lang="ts">
+	import type { Readable } from 'svelte/store';
+
+	import { toast } from 'svelte-sonner';
 	import { source } from 'sveltekit-sse';
 	import { Button } from '$lib/components/ui';
 	import { Play, Square } from 'lucide-svelte';
 
-	let value = $state(source('/api/simulation').select('message'));
+	const connection = source('/api/simulation');
+
+	let onProgress: boolean = $state(false);
+
+	let initConnection: Readable<string> | undefined = $state();
+	let agentsCoordinates: Readable<[number, number][]> | undefined = $state();
+
+	$effect(() => {
+		if ($initConnection === 'success') {
+			toast.loading('Iniciando simulación...');
+			onProgress = true;
+		}
+	});
+
+	$effect(() => {
+		if (onProgress) {
+			agentsCoordinates = connection.select('agentsCoordinates').transform(() => {
+				return [[-33.015348, -71.550002]];
+			});
+		}
+	});
+
+	function startSimulation() {
+		initConnection = connection.select('initConnection');
+	}
+
+	// TODO: Implement stop simulation
+	function stopSimulation() {
+		onProgress = false;
+	}
 </script>
 
-<Button>
-	<Play class="mr-1.5 size-4" />
-	<span>Iniciar</span>
-</Button>
+{#if !onProgress}
+	<Button onclick={startSimulation} disabled={onProgress}>
+		<Play class="mr-1.5 size-4" />
+		<span>Iniciar</span>
+	</Button>
+{:else}
+	<Button onclick={stopSimulation}>
+		<Square class="mr-1.5 size-4" />
+		<span>Detener</span>
+	</Button>
+{/if}
 
-<Button>
-	<Square class="mr-1.5 size-4" />
-	<span>Detener</span>
-</Button>
-
-<p>
-	value: {$value}
-</p>
+{#if $initConnection === 'success' && onProgress}
+	{#await import('$lib/components/leaflet/map/map.svelte') then Map}
+		{#await import('$lib/components/leaflet/mask-canvas/mask-canvas.svelte') then Canvas}
+			<svelte:component this={Map.default}>
+				{#if $agentsCoordinates}
+					<svelte:component this={Canvas.default} coordinates={$agentsCoordinates} />
+				{/if}
+			</svelte:component>
+		{/await}
+	{/await}
+{/if}
