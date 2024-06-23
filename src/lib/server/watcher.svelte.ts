300332d661f77ca5ee0f7c5936f1e231fa91a1c9
Ariel Salgado <ariel.salgado.acevedo@gmail.com>
2024-06-23 08:21:41 +0000

    Unique instance handled on invoke

--- src/lib/server/watcher.svelte.ts
+++ src/lib/server/watcher.svelte.ts
@@ -1,11 +1,20 @@
-import { watch } from 'chokidar';
+import type { FSWatcher } from 'chokidar';
 
-export function createWatcher(path: string) {
+import { watch } from 'chokidar';
+import { uniquePool } from '$lib/states';
+
+export function createWatcher(id: string, path: string) {
 	const watcher = $state(
 		watch(path, {
 			ignoreInitial: true
 		})
 	);
+
+	if (uniquePool.has(id)) {
+		uniquePool.pop<FSWatcher>(id)?.close();
+	}
+
+	uniquePool.add(id, watcher);
 
 	return watcher;
 }
