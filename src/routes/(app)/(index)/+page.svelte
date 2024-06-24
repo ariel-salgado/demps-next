1a333a84411904eb266d9c4576539402cd3815ae
Ariel Salgado <ariel.salgado.acevedo@gmail.com>
2024-06-24 01:44:18 +0000

    Added index page view

--- src/routes/(app)/(index)/+page.svelte
+++ src/routes/(app)/(index)/+page.svelte
@@ -1,4 +1,11 @@
-<script lang="ts">
-</script>
+<svelte:head>
+	<title>DEMPS | Inicio</title>
+	<meta name="description" content="DEMPS | Inicio" />
+</svelte:head>
 
-index
+<section class="grid h-full place-items-center items-center justify-items-center">
+	<div class="-mt-40 text-center">
+		<h1 class="text-6xl font-bold">DEMPS</h1>
+		<p class="py-1 text-lg text-zinc-500">Disaster Evacuation and Mobility Patterns Simulator.</p>
+	</div>
+</section>
