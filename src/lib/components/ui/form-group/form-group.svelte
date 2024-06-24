2aaf7e5763d9b884b405396599cbb29e3abf3636
Ariel Salgado <ariel.salgado.acevedo@gmail.com>
2024-06-24 06:55:40 +0000

    Added form group component

---
+++ src/lib/components/ui/form-group/form-group.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+	import type { Snippet } from 'svelte';
+	import type { HTMLAttributes } from 'svelte/elements';
+
+	import { cn } from '$lib/utils';
+
+	interface Props extends HTMLAttributes<HTMLDivElement> {
+		children: Snippet;
+	}
+
+	let { children, class: className, ...props }: Props = $props();
+</script>
+
+<div
+	class={cn(
+		'space-y-2 rounded-md py-3.5 px-5 transition-colors focus-within:bg-zinc-50 hover:bg-zinc-50 has-[.error]:bg-red-600/10 has-[.error]:focus-within:bg-red-600/10 has-[.error]:hover:bg-red-600/10 has-[input:read-only]:bg-zinc-100 has-[select:disabled]:bg-zinc-100',
+		className
+	)}
+	{...props}
+>
+	{@render children()}
+</div>
--- src/lib/components/ui/index.ts
+++ src/lib/components/ui/index.ts
@@ -2,6 +2,7 @@ import { Label } from './label';
 import { Input } from './input';
 import { Header } from './header';
 import { Button } from './button';
+import { FormGroup } from './form-group';
 import { SplitView } from './split-view';
 
-export { Input, Label, Header, Button, SplitView };
+export { Input, Label, Header, Button, FormGroup, SplitView };
