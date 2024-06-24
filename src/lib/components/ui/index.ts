29a3e4713cf97d317501af12e49c2e5b7577fedc
Ariel Salgado <ariel.salgado.acevedo@gmail.com>
2024-06-24 06:58:50 +0000

    Added description component

--- src/lib/components/ui/index.ts
+++ src/lib/components/ui/index.ts
@@ -4,5 +4,6 @@ import { Header } from './header';
 import { Button } from './button';
 import { FormGroup } from './form-group';
 import { SplitView } from './split-view';
+import { Description } from './description';
 
-export { Input, Label, Header, Button, FormGroup, SplitView };
+export { Input, Label, Header, Button, FormGroup, SplitView, Description };
