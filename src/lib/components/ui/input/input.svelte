c21c2925989c628f4d0abd59f3ab9c16cecf4140
Ariel Salgado <ariel.salgado.acevedo@gmail.com>
2024-07-07 19:10:08 -0400

    Fix non numeric values in number type input

--- src/lib/components/ui/input/input.svelte
+++ src/lib/components/ui/input/input.svelte
@@ -1,15 +1,22 @@
 <script lang="ts">
 	import type { z } from 'zod';
+	import type { Action } from 'svelte/action';
 	import type { HTMLInputAttributes } from 'svelte/elements';
 
 	import { cn } from '$lib/utils';
-	import { baseStyle, initInput } from './props.svelte';
+	import { baseStyle, onlyAllowNumbers } from './props';
 
 	interface Props extends HTMLInputAttributes {
 		validation?: z.ZodType;
 	}
 
-	let { value: value = $bindable(), class: className, validation, ...rest }: Props = $props();
+	let {
+		type: type,
+		value: value = $bindable(),
+		class: className,
+		validation,
+		...rest
+	}: Props = $props();
 
 	let validationError: boolean = $state(false);
 	let inputError: string | undefined = $state();
@@ -28,13 +35,28 @@
 		inputError = error.format()._errors.at(0);
 		return;
 	}
+
+	const initInput: Action<HTMLInputElement> = (element: HTMLInputElement) => {
+		if (type !== 'number') element.type = 'text';
+
+		if (type === 'number') {
+			element.addEventListener('keydown', onlyAllowNumbers);
+		}
+
+		return {
+			destroy() {
+				element.removeEventListener('keydown', onlyAllowNumbers);
+				element.remove();
+			}
+		};
+	};
 </script>
 
 <input
+	{...rest}
 	class={cn(baseStyle, className)}
 	class:error={validationError}
 	onchange={validation ? validateField : undefined}
-	{...rest}
 	use:initInput
 	bind:value
 />
