474ac1137ed022b31449d8f803fcf00c629b0119
Ariel Salgado <ariel.salgado.acevedo@gmail.com>
2024-07-07 19:12:01 -0400

    Set as default value extractDefaultValues in parameters state

--- src/lib/states/index.ts
+++ src/lib/states/index.ts
@@ -1,4 +1,6 @@
 import { createUniquePool } from './pool.svelte';
+import { extractDefaultValues } from '$lib/utils';
+import { parametersFormFields } from '$lib/config';
 import { createParameters } from './parameters.svelte';
 import { createEnvironment } from './environment.svelte';
 
@@ -7,5 +9,5 @@ export type Parameters = ReturnType<typeof createParameters>;
 export type Environment = ReturnType<typeof createEnvironment>;
 
 export const uniquePool = createUniquePool();
-export const parameters = createParameters();
 export const environment = createEnvironment();
+export const parameters = createParameters(extractDefaultValues(parametersFormFields));
