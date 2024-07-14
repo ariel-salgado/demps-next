8c8bde6162dcdbb17830cd30261f7c16c709f99e
Ariel Salgado <ariel.salgado.acevedo@gmail.com>
2024-07-14 05:44:58 -0400

    Changed spacing

--- src/routes/(app)/(index)/+page.svelte
+++ src/routes/(app)/(index)/+page.svelte
@@ -5,7 +5,7 @@
 
 <section class="grid h-full place-items-center items-center justify-items-center">
 	<div class="-mt-40 text-center">
-		<h1 class="text-6xl font-bold">DEMPS</h1>
-		<p class="py-1 text-lg text-slate-500">Disaster Evacuation and Mobility Patterns Simulator.</p>
+		<h1 class="text-6xl font-bold leading-tight">DEMPS</h1>
+		<p class="text-lg text-slate-500">Disaster Evacuation and Mobility Patterns Simulator.</p>
 	</div>
 </section>
