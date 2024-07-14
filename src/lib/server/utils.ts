2aa2f005e00f94028bc715a90563102c96135b23
Ariel Salgado <ariel.salgado.acevedo@gmail.com>
2024-07-14 05:44:39 -0400

    Added more file operations

--- src/lib/server/utils.ts
+++ src/lib/server/utils.ts
@@ -1,49 +1,72 @@
-import { join } from 'node:path';
+import type { FetchDirectoryOptions } from '$lib/types';
+
+import { extname, join } from 'node:path';
 import { existsSync, mkdirSync, readdirSync, rm, statSync } from 'node:fs';
 
-export function directoryExists(path: string) {
+export function isDirectory(path: string) {
 	return existsSync(path) && statSync(path).isDirectory();
 }
 
-export function getDirectories(path: string) {
-	const folders: string[] = [];
-
-	const files = readdirSync(path);
-	for (const file of files) {
-		const fullPath = join(path, file);
-		if (directoryExists(fullPath)) {
-			folders.push(fullPath);
-		}
-	}
-
-	return folders;
+export function isFile(path: string) {
+	return existsSync(path) && statSync(path).isFile();
 }
 
-export function getDirectoryTree(path: string) {
+export function getDirectoryContents(
+	path: string,
+	options: FetchDirectoryOptions = { extensions: null, includeFiles: true, includeFolders: true }
+) {
+	if (!isDirectory(path)) {
+		return { files: [], folders: [] };
+	}
+
+	const { extensions, includeFiles, includeFolders } = options;
+
+	const files: string[] = [];
 	const folders: string[] = [];
 
-	function traverseDir(currentPath: string) {
-		const files = readdirSync(currentPath);
+	const contents = readdirSync(path);
 
-		for (const file of files) {
-			const fullPath = join(currentPath, file);
-			if (directoryExists(fullPath)) {
-				folders.push(fullPath);
-				traverseDir(fullPath);
+	for (const item of contents) {
+		const fullPath = join(path, item);
+		const stat = statSync(fullPath);
+
+		if (stat.isDirectory() && includeFolders) {
+			folders.push(item);
+		} else if (stat.isFile() && includeFiles) {
+			if (
+				!extensions ||
+				extensions.length === 0 ||
+				extensions.includes(extname(item).toLowerCase())
+			) {
+				files.push(item);
 			}
 		}
 	}
 
-	traverseDir(path);
-	return folders;
+	return { files, folders };
 }
 
 export function createDirectory(path: string) {
 	return existsSync(path) || mkdirSync(path);
 }
 
+export function deleteFile(path: string) {
+	if (!isFile(path)) {
+		return false;
+	}
+
+	rm(path, { recursive: false, force: false }, (error) => {
+		if (error) {
+			console.error(error);
+			return false;
+		}
+	});
+
+	return true;
+}
+
 export function deleteDirectory(path: string) {
-	if (!directoryExists(path)) {
+	if (!isDirectory(path)) {
 		return false;
 	}
 
