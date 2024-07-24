230e3be19cb266e44461cd76cffabb14073ea8f7
Ariel Salgado <ariel.salgado.acevedo@gmail.com>
2024-07-24 01:41:34 -0400

    Handling server config file loading

--- src/routes/api/simulator/setup/+server.ts
+++ src/routes/api/simulator/setup/+server.ts
@@ -4,7 +4,7 @@ import type { ParametersSchema } from '$lib/types';
 import { json } from '@sveltejs/kit';
 import { fileURLToPath } from 'node:url';
 import { dirname, join, sep } from 'node:path';
-import { createFile, deleteFile, isFile } from '$lib/server/utils';
+import { createFile, deleteFile, isFile, readFile } from '$lib/server/utils';
 import { defaultParametersConfigFilename, parametersFormFields } from '$lib/config';
 import {
 	deflattenJSON,
@@ -25,6 +25,8 @@ export const POST = (async ({ request }) => {
 		});
 	}
 
+	let parameters: ParametersSchema;
+
 	if (type === 'local') {
 		const { success: validConfig, errors: configErrors } = verifyIntegrity(config);
 
@@ -37,55 +39,99 @@ export const POST = (async ({ request }) => {
 			});
 		}
 
-		const parameters = preprocessParametersData(deflattenJSON(config) as ParametersSchema);
-
-		const {
-			baseDirSim,
-			floodModelEnable,
-			input: { directory: inputDirectory, zones },
-			output: {
-				directory: outputDirectory,
-				'agents-out': agentsOut,
-				'agents-path': agentsPath,
-				'agents-sufix': agentsSufix
-			},
-			floodParams: { stateEnable, stateDir }
-		} = parameters;
-
-		const fullInputDirectory = join(baseDirSim, inputDirectory);
-		const fullOutputDirectory = join(baseDirSim, outputDirectory);
-		const enableFloodWatcher = floodModelEnable && stateEnable;
-		const floodDir = join(fullOutputDirectory, stateDir);
-		const agentsDir = join(fullOutputDirectory, agentsPath);
-
-		const simulatorDirectives = {
-			baseDirSim,
-			zones,
-			floodEnabled: enableFloodWatcher,
-			inputDirectory: fullInputDirectory,
-			outputDirectory: fullOutputDirectory,
-			agentsOut,
-			agentsDir,
-			agentsSufix,
-			floodDir
-		};
-
-		const iniFileCreated = createSimulatorLaunchFile(simulatorDirectives);
-
-		if (!iniFileCreated) {
+		parameters = preprocessParametersData(deflattenJSON(config) as ParametersSchema);
+	} else if (type === 'server') {
+		if (!isFile(config)) {
 			return json({
 				error: {
-					code: 500,
-					message: 'No se pudo crear el archivo para iniciar el simulador.'
+					code: 404,
+					message: 'No se encuentra el archivo de configuración seleccionado.'
 				}
 			});
 		}
 
-		// Create simulator config file
+		const configData = readFile(config);
+
+		if (!configData) {
+			return json({
+				error: {
+					code: 400,
+					message: 'No se pudo obtener la información del archivo de configuración seleccionado.'
+				}
+			});
+		}
+
+		try {
+			parameters = JSON.parse(configData);
+		} catch {
+			return json({
+				error: {
+					code: 400,
+					message: 'El archivo de configuración seleccionado no es válido.'
+				}
+			});
+		}
+	} else {
+		return json({
+			error: {
+				code: 400,
+				message: 'Hubo un error enviando la información al servidor.'
+			}
+		});
+	}
+
+	const {
+		baseDirSim,
+		floodModelEnable,
+		input: { directory: inputDirectory, zones },
+		output: {
+			directory: outputDirectory,
+			'agents-out': agentsOut,
+			'agents-path': agentsPath,
+			'agents-sufix': agentsSufix
+		},
+		floodParams: { stateEnable, stateDir }
+	} = parameters;
+
+	const fullInputDirectory = join(baseDirSim, inputDirectory);
+	const fullOutputDirectory = join(baseDirSim, outputDirectory);
+	const enableFloodWatcher = floodModelEnable && stateEnable;
+	const floodDir = join(fullOutputDirectory, stateDir);
+	const agentsDir = join(fullOutputDirectory, agentsPath);
+	const configFile = type === 'local' ? defaultParametersConfigFilename : config;
+
+	const simulatorDirectives = {
+		configFile,
+		baseDirSim,
+		zones,
+		floodEnabled: enableFloodWatcher,
+		inputDirectory: fullInputDirectory,
+		outputDirectory: fullOutputDirectory,
+		agentsOut,
+		agentsDir,
+		agentsSufix,
+		floodDir
+	};
+
+	// Create simulator ini file
+	const iniFileCreated = createSimulatorLaunchFile(simulatorDirectives);
+
+	if (!iniFileCreated) {
+		return json({
+			error: {
+				code: 500,
+				message: 'No se pudo crear el archivo para iniciar el simulador.'
+			}
+		});
+	}
+
+	// Create simulator config file
+	if (type === 'local') {
 		const configFileCreated = createFile(
 			baseDirSim,
-			defaultParametersConfigFilename,
-			JSON.stringify(parameters, null, '\t')
+			configFile,
+			JSON.stringify(parameters, null, '\t'),
+			true
 		);
 
 		if (!configFileCreated) {
@@ -96,13 +142,9 @@ export const POST = (async ({ request }) => {
 				}
 			});
 		}
-
-		return json({ directives: simulatorDirectives });
 	}
 
-	// TODO: Implement remote setup
-
-	return json({ content: 'hola' });
+	return json({ directives: simulatorDirectives });
 }) satisfies RequestHandler;
 
 function verifyIntegrity(config: Record<string, unknown>) {
