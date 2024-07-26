47a3e323e834f70e1a7698f0e1dc90e4cef3d165
Ariel Salgado <ariel.salgado.acevedo@gmail.com>
2024-07-26 19:22:05 -0400

    Create app-zones.geojson if it doesnt exist on load, so there is a file to pick

--- src/routes/(app)/parametros/+page.server.ts
+++ src/routes/(app)/parametros/+page.server.ts
@@ -1,9 +1,31 @@
 import type { Actions } from '@sveltejs/kit';
+import type { PageServerLoad } from './$types';
 
+import { join } from 'node:path';
 import { getValidationSchema } from '$lib/utils';
 import { parametersFormFields } from '$lib/config';
+import { PUBLIC_SIM_DIR } from '$env/static/public';
+import { createFile, isFile } from '$lib/server/utils';
 
 export const prerender = false;
+
+export const load = (async () => {
+	const path: string = join(PUBLIC_SIM_DIR, 'input/');
+	const file: string = 'app-zones.geojson';
+
+	if (isFile(join(path, file))) return;
+
+	const defaultData: string = JSON.stringify(
+		{
+			type: 'FeatureCollection',
+			features: []
+		},
+		null,
+		'\t'
+	);
+
+	createFile(path, file, defaultData);
+}) satisfies PageServerLoad;
 
 // TODO: Remove duplicates, better handling on client-side code
 export const actions = {
