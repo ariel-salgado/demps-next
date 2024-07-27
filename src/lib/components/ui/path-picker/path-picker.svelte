fcda327b781ea0cd35ae3fbcb8fb2741ebe75945
Ariel Salgado <ariel.salgado.acevedo@gmail.com>
2024-07-26 20:25:45 -0400

    Added path picker

---
+++ src/lib/components/ui/path-picker/path-picker.svelte
@@ -0,0 +1,131 @@
+<script lang="ts">
+	import type { Action } from 'svelte/action';
+	import type { PathPickerProps } from '$lib/types';
+	import type { HTMLInputAttributes } from 'svelte/elements';
+
+	import { cn } from '$lib/utils';
+	import { on } from 'svelte/events';
+	import { Dialog } from '$lib/components/ui';
+	import { File, Folder } from 'lucide-svelte';
+	import { Explorer } from '$lib/components/file-explorer';
+
+	type Props = PathPickerProps & HTMLInputAttributes;
+
+	let {
+		basePath,
+		initialPath,
+		validation,
+		onSelected,
+		extensions = null,
+		includeFiles = true,
+		includeFolders = true,
+		isFile = $bindable(false),
+		disableBacktracking = false,
+		value: value = $bindable(null),
+		class: className,
+		...rest
+	}: Props = $props();
+
+	let showDialog: boolean = $state(false);
+	let validationError: boolean = $state(false);
+
+	let inputError: string | undefined = $state();
+	let inputElement: HTMLInputElement | undefined = $state();
+
+	function handleSelected() {
+		showDialog = false;
+		inputElement!.scrollLeft = inputElement!.scrollWidth;
+
+		if (validation) {
+			validateField();
+		}
+
+		if (onSelected) {
+			onSelected();
+		}
+	}
+
+	function validateField() {
+		if (!validation) return;
+
+		const { success, error } = validation.safeParse(value);
+
+		if (success) {
+			validationError = false;
+			return;
+		}
+
+		validationError = true;
+		inputError = error.format()._errors.at(0);
+	}
+
+	function getFolderPath(path: string) {
+		if (!isFile) return path;
+
+		return path.replace(/[^/]*$/, '').replace(/\/$/, '');
+	}
+
+	const initPathPicker: Action<HTMLInputElement> = (inputElement: HTMLInputElement) => {
+		const onFocus = on(inputElement, 'focus', () => {
+			showDialog = true;
+		});
+
+		$effect(() => {
+			inputElement.scrollLeft = inputElement.scrollWidth;
+		});
+
+		return {
+			destroy() {
+				onFocus();
+			}
+		};
+	};
+</script>
+
+<Dialog
+	bind:show={showDialog}
+	onClose={() => (inputElement!.scrollLeft = inputElement!.scrollWidth)}
+>
+	<Explorer
+		{basePath}
+		directory={initialPath || getFolderPath(value)}
+		bind:selected={value}
+		bind:isFile
+		{extensions}
+		{includeFolders}
+		{includeFiles}
+		onSelected={handleSelected}
+		{disableBacktracking}
+	/>
+</Dialog>
+
+<div
+	class:error={validationError}
+	class={cn(
+		'flex h-9 items-center divide-x divide-slate-300 overflow-hidden rounded-md border border-slate-300 bg-white text-sm ring-offset-white focus-within:ring-2 focus-within:ring-slate-700 focus-within:ring-offset-2 focus-within:outline-none',
+		`${validationError ? 'border-2 border-red-600 focus-within:ring-red-600' : ''}`,
+		className
+	)}
+>
+	<span class="flex size-9 items-center justify-center rounded-l-md bg-slate-200/80 p-2">
+		{#if isFile}
+			<File class="size-[18px]" />
+		{:else}
+			<Folder class="size-[18px]" />
+		{/if}
+	</span>
+	<input
+		bind:this={inputElement}
+		class="w-full border-none px-3 focus-within:outline-none"
+		{...rest}
+		use:initPathPicker
+		bind:value
+		onkeydown={function (e) {
+			e.preventDefault();
+		}}
+	/>
+</div>
+
+{#if validationError}
+	<small class="text-red-600">{inputError}</small>
+{/if}
