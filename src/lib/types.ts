318249378c5da08ce29ee23ba5f91f4ded369186
Ariel Salgado <ariel.salgado.acevedo@gmail.com>
2024-07-29 04:08:54 -0400

    Added DeepPartial

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -6,6 +6,10 @@ import type {
 	HTMLSelectAttributes
 } from 'svelte/elements';
 
+export type DeepPartial<T> = {
+	[K in keyof T]?: T[K] extends object ? DeepPartial<T[K]> : T[K];
+};
+
 export type FetchDirectoryOptions = Partial<{
 	extensions: string[] | null;
 	includeFiles: boolean;
