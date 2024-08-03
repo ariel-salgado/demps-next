866d28beeb7be65eb5f4bdd18c79eb116efd8f8b
Ariel Salgado <ariel.salgado.acevedo@gmail.com>
2024-08-03 03:35:10 -0400

    Added current editing file message

--- src/routes/(app)/entorno/+page.svelte
+++ src/routes/(app)/entorno/+page.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-	import { environment } from '$lib/states';
+	import { createPersisted, environment, parameters } from '$lib/states';
 	import { SplitView } from '$lib/components/ui';
 	import { Map, ToggleLayers, Geoman, Geosearch } from '$lib/components/leaflet';
 	import {
@@ -14,8 +14,15 @@
 	let reload: boolean = $state(false);
 	let files: FileList | null = $state(null);
 
+	let zoneFile = createPersisted('zone', parameters.value.input?.zones);
+
 	function triggerFullReload() {
 		reload = true;
+	}
+
+	function handleOnLoadedFile(selectedFile: string) {
+		zoneFile.value = selectedFile;
+		triggerFullReload();
 	}
 </script>
 
@@ -24,7 +31,7 @@
 	<meta name="description" content="Environment" />
 </svelte:head>
 
-<section class="size-full">
+<section class="isolate size-full">
 	<SplitView>
 		{#snippet left()}
 			<Map {environment} bind:reload isLayerEditable>
@@ -37,10 +44,17 @@
 			<Editor {environment} onChanges={triggerFullReload}>
 				<DownloadFile />
 				<CopyToClipboard />
-				<LoadData onLoad={triggerFullReload} />
+				<LoadData onLoad={(selectedFile) => handleOnLoadedFile(selectedFile)} />
 				<ClearData onClear={triggerFullReload} />
 				<UploadFile accept=".geojson" {files} onUpload={triggerFullReload} />
 			</Editor>
 		{/snippet}
 	</SplitView>
+
+	{#if zoneFile.value}
+		<span
+			class="absolute bottom-0 left-0 rounded-tr-lg border border-t-slate-500 border-r-slate-500 border-b-transparent border-l-transparent bg-white py-0.5 px-3 text-sm"
+			>Editando: {zoneFile.value}</span
+		>
+	{/if}
 </section>
