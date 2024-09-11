880b2b62722522c0b7f9cc32666113d04408875c
Ariel Salgado <ariel.salgado.acevedo@gmail.com>
2024-09-11 04:14:31 -0300

    Remove unused code

--- src/lib/server/helpers/index.ts
+++
@@ -1,3 +0,0 @@
-import { create_agent_processor, create_flood_processor } from './data-processors';
-
-export { create_agent_processor, create_flood_processor };
--- src/lib/server/index.ts
+++
@@ -1,6 +0,0 @@
-import { createWatcher } from './watcher.svelte';
-import { createFileProcessor } from './file-processor.svelte';
-
-export type Watcher = ReturnType<typeof createWatcher>;
-
-export { createWatcher, createFileProcessor };
