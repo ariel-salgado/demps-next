727fff53c4b6d1d8df91ffd9257b87eadfd1c5f1
Ariel Salgado <ariel.salgado.acevedo@gmail.com>
2024-09-11 00:00:55 -0300

    Remove unused code

--- src/lib/server/index.ts
+++ src/lib/server/index.ts
@@ -1,8 +1,6 @@
-import { createDempsProcess } from './simulator.svelte';
 import { createWatcher } from './watcher.svelte';
 import { createFileProcessor } from './file-processor.svelte';
 
 export type Watcher = ReturnType<typeof createWatcher>;
-export type DempsProcess = ReturnType<typeof createDempsProcess>;
 
-export { createWatcher, createDempsProcess, createFileProcessor };
+export { createWatcher, createFileProcessor };
