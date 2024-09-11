2437b5816a6fcd3fef48112a08af4c4ac5b02753
Ariel Salgado <ariel.salgado.acevedo@gmail.com>
2024-09-10 23:58:10 -0300

    Remove server hmr

--- vite.config.ts
+++ vite.config.ts
@@ -6,14 +6,12 @@ import tailwindcss from '@tailwindcss/vite';
 export default defineConfig({
 	plugins: [sveltekit(), tailwindcss()],
 	server: {
-		hmr: true,
 		host: '0.0.0.0'
 	},
 	build: {
 		cssMinify: 'lightningcss'
 	},
 	test: {
-		globals: true,
 		environment: 'node',
 		include: ['src/**/*.{test,spec}.{js,ts}']
 	}
